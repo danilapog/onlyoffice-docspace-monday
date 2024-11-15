25f012a2bbdc8f0e9a7393412b1d6115668b4555
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-11-16 00:40:29 +0300

    fix: enable method security

--- onlyoffice-monday-gateway/src/main/java/com/onlyoffice/gateway/GatewayServerApplication.java
+++ onlyoffice-monday-gateway/src/main/java/com/onlyoffice/gateway/GatewayServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @EnableWebSocket
@@ -13,6 +14,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties
+@EnableMethodSecurity(securedEnabled = true)
 @SpringBootApplication(scanBasePackages = {"com.onlyoffice.gateway", "com.onlyoffice.common"})
 public class GatewayServerApplication {
   public static void main(String[] args) {
