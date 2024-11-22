1de62c0f79205037d5682226b3194c9d2e292ec1
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-11-22 19:43:00 +0300

    feat(tests): tenant query controller

---
+++ onlyoffice-monday-tenant/src/test/java/com/onlyoffice/tenant/controller/query/TenantQueryControllerTest.java
@@ -0,0 +1,63 @@
+package com.onlyoffice.tenant.controller.query;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlyoffice.common.tenant.transfer.request.query.FindEntity;
+import com.onlyoffice.common.tenant.transfer.response.TenantCredentials;
+import com.onlyoffice.tenant.controller.GlobalControllerAdvice;
+import com.onlyoffice.tenant.service.query.TenantQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@ExtendWith(MockitoExtension.class)
+public class TenantQueryControllerTest {
+  private MockMvc mvc;
+  @Mock private TenantQueryService queryService;
+  @InjectMocks private TenantQueryController controller;
+
+  @BeforeEach
+  public void setup() {
+    JacksonTester.initFields(this, new ObjectMapper());
+    mvc =
+        MockMvcBuilders.standaloneSetup(controller)
+            .setControllerAdvice(new GlobalControllerAdvice())
+            .setValidator(new LocalValidatorFactoryBean())
+            .build();
+  }
+
+  @Test
+  void shouldFindTenant_WhenValidTenantId_ThenReturnTenantCredentials() throws Exception {
+    var tenantId = 1;
+    var tenantCredentials =
+        TenantCredentials.builder()
+            .id(tenantId)
+            .docSpaceUrl("https://example.com")
+            .docSpaceLogin("admin@admin.com")
+            .docSpaceHash("hash")
+            .build();
+
+    Mockito.when(queryService.find(Mockito.any(FindEntity.class))).thenReturn(tenantCredentials);
+
+    var response =
+        mvc.perform(get("/tenants/{tenantId}", tenantId).accept(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse();
+
+    Mockito.verify(queryService)
+        .find(Mockito.argThat(findEntity -> findEntity.getId() == tenantId));
+    assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+}
